fdd65db9ad40d8b085e2439ac694b581e8b86f68
ninadkadam123 <ninadkadam123@gmail.com>
2024-09-20 17:08:44 +0530

    vcapp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Videocall from './components/videocall';
+import CreateMeeting from './components/createmeeting';
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+} from 'react-router-dom';
 function App() {
-  return <div className="App">Hii</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CreateMeeting />}></Route>
+        <Route path="/room" element={<Videocall />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/videocall.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { v4 as uuidv4 } from 'uuid';
+const Videocall = () => {
+  const location = useLocation();
+  const queryParameter = new URLSearchParams(location.search);
+  const roomID = queryParameter.get('roomId');
+
+  let myMeeting = async (element) => {
+    const appID = await process.env.REACT_APP_ZEGOCLOUD_ID;
+    const serverSecret = await process.env.REACT_APP_ZEGOCLOUD_SECRET;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      parseInt(appID),
+      serverSecret,
+      roomID,
+      uuidv4(),
+      uuidv4()
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    zp.joinRoom({
+      sharedLinks: [
+        {
+          name: 'Meeting Link',
+          url: `http://localhost:3000/room?roomId=${roomID}`,
+        },
+      ],
+      container: element,
+      scenario: { mode: ZegoUIKitPrebuilt.OneONoneCall },
+    });
+  };
+
+  return (
+    <div
+      ref={myMeeting}
+      style={{ width: '100%', height: '100vh' }}
+    ></div>
+  );
+};
+
+export default Videocall;
